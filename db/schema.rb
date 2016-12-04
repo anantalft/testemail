c3411460a09f4b02b899f8fb37a657a0e0e1e45f
anantalamichhane <anantalamichhane1@gmail.com>
2016-12-04 13:43:57 +0545

    db updated to pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203110158) do
+ActiveRecord::Schema.define(version: 20161204071447) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "pages", force: :cascade do |t|
     t.string   "user"
     t.text     "template"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
